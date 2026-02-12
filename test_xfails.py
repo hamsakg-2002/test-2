25ac8cd7a0ecb6774239a32fd951349596c79c3a
Hamsa <hamsakgkg@gmail.com>
2026-02-12 11:19:16 +0530

    testing auto trigger

--- test_xfails.py
+++ test_xfails.py
@@ -5,7 +5,7 @@ def s_list():
     return [1,2,3,4]
 @pytest.mark.xfail(reason="Known bug: wrong expected max value")
 def test_max_list(s_list):
-    assert max([1,2,3,4]) == 2
+    assert max([1,2,3,4]) == 3
 def test_min_list(s_list):
     assert min([1, 2, 3, 4]) == 1
 
