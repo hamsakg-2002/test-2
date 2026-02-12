26adef7871da18ff9ea4f51ed896bed4345ba5c5
Hamsa <hamsakgkg@gmail.com>
2026-02-12 09:44:13 +0530

    Initial commit: add test_xfails.py

---
+++ test_xfails.py
@@ -0,0 +1,13 @@
+# using fixtures
+import pytest
+@pytest.fixture
+def s_list():
+    return [1,2,3,4]
+@pytest.mark.xfail(reason="Known bug: wrong expected max value")
+def test_max_list(s_list):
+    assert max([1,2,3,4]) == 1
+def test_min_list(s_list):
+    assert min([1, 2, 3, 4]) == 1
+
+
+
